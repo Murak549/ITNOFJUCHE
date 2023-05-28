dbf9a83f14a9e02c5b783d7ad2694b2062c918a5
i <51825158+Murak549@users.noreply.github.com>
2023-05-27 23:18:46 -0700

    연락사무소 폭파

--- 남북연락사무소(Tools)/musicat.py
+++
@@ -1,7 +0,0 @@
-from pytube import YouTube
-
-url = input('Give me a link: ')
-
-yt = YouTube(url)
-# print(yt.title)
-# print(yt.thumbnail_url)
--- 남북연락사무소(Tools)/sex_with_nazi.py
+++
@@ -1,6 +0,0 @@
-p = int(input("숫자를 입력하세요: "))
-q = int(input("최대값을 입력하세요: "))
-
-for num in range(p, q+1):
-    state = num
-    print("state = ", state)
