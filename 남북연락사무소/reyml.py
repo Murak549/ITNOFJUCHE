03a3a2d6740ae74216527d376f18567966064a26
Murak549 <murak1203@gmail.com>
2023-10-12 17:38:35 +0900

    Jew Jew Jew

--- 남북연락사무소/reyml.py
+++ 남북연락사무소/reyml.py
@@ -1,22 +1,10 @@
-import re
-import os
+with open('NKBK_state_names_l_english.yml', 'r', encoding='UTF8') as file:
+    lines = file.readlines()
 
-# 현재 디렉토리의 파일 목록을 가져옵니다.
-files = os.listdir()
-
-for filename in files:
-    # 파일을 엽니다.
-    with open(filename, 'r+', encoding='UTF8') as file:
-        lines = file.readlines()
-        # 변경된 내용을 저장할 리스트를 만듭니다.
-        new_lines = []
-        for line in lines:
-            # manpower= 행은 제외합니다.
-            if not line.strip().startswith('manpower=' or 'manpower ='):
-                # 909 이상의 숫자를 찾아서 36를 더합니다.
-                line = re.sub(r'\b(\d{5,})\b', lambda m: str(int(m.group(1)) + 36 if int(m.group(1)) >= 909 else m.group(1)), line)
-            new_lines.append(line)
-        # 변경된 내용으로 파일을 덮어씁니다.
-        file.seek(0)
-        file.writelines(new_lines)
-        file.truncate()
+with open('NKBK_state_names_l_english.yml', 'w', encoding='UTF8') as file:
+    for line in lines:
+        if line.startswith(' STATE_'):
+            state_number = int(line.split(':')[0].split('_')[1])
+            if state_number >= 909:
+                line = ' STATE_' + str(state_number + 26) + line.split(':')[1]
+        file.write(line)
